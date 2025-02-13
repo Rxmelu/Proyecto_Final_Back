3ddd7026c7deb7fad0d0acc7a4d2fb9e2f828715
Rxmelu <romeorcosta@gmail.com>
2025-02-13 09:35:57 +0100

    link_actualizado

--- src/db-connections.ts
+++ src/db-connections.ts
@@ -8,8 +8,7 @@ import { Pool } from 'pg';
     database: 'basedatos_final'
 });*/
 
-const connectionString = 
-'postgresql://finalproyect_4cy3_user:LqY9r8LWN6uSirYMoAQAb3qHFiHB5lfV@dpg-cum655l6l47c7395btr0-a.frankfurt-postgres.render.com/finalproyect_4cy3'
+const connectionString = 'postgresql://finalproyect_4cy3_user:LqY9r8LWN6uSirYMoAQAb3qHFiHB5lfV@dpg-cum655l6l47c7395btr0-a/finalproyect_4cy3'
 
 const pool = new Pool({
     connectionString,
